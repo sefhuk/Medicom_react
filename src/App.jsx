13ed86eb381f495aba36a169f9855a769bef02c1
sanghyuk <ish775800@gmail.com>
2024-07-23 10:56:12 +0900

    fix: 기본 레이아웃 렌더링 문제 해결

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import './App.css';
 import { Outlet } from 'react-router-dom';
-import Footer from './components/Footer';
 
 function App() {
   return (
     <div>
       <Outlet />
-      <Footer />
     </div>
   );
 }
--- src/components/global/MainContainer.jsx
+++ src/components/global/MainContainer.jsx
@@ -1,19 +1,20 @@
 import React from 'react';
 import styled from 'styled-components';
 import Nav from '../Nav';
+import Footer from '../Footer';
 
 function MainContainer({ children }) {
   return (
     <Container>
       <Nav />
       {children}
+      <Footer />
     </Container>
   );
 }
 
 const Container = styled.div`
   height: 92dvh;
-  background-color: yellow;
   overflow-y: scroll;
 `;
 
