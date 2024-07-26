e64b51eb3f7d67702b34b709a2832f1c5612aebf
sanghyuk <ish775800@gmail.com>
2024-07-26 16:19:55 +0900

    refactor: React Query -> 단순 Axios

--- src/pages/chat/ChatListPage.jsx
+++ src/pages/chat/ChatListPage.jsx
@@ -1,44 +1,47 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import MainContainer from '../../components/global/MainContainer';
 import { axiosInstance } from '../../utils/axios';
-import { useQuery } from '@tanstack/react-query';
 import styled from 'styled-components';
 import ChatRoomDetail from '../../components/chatRoom/ChatRoomDetail';
 import { useRecoilState } from 'recoil';
 import { authState, chatRoomState } from '../../utils/atom';
 
-const fetchData = async (id, setChatRoom) => {
-  try {
-    const response = await axiosInstance.get(`/chatrooms`, {
-      params: {
-        userId: id
-      }
-    });
-    const chatRooms = {};
-    response.data.map(e => (chatRooms[`ch_${e.id}`] = e.status.status));
-    setChatRoom({ rooms: chatRooms });
-    return response.data;
-  } catch (err) {
-    throw new Error(err.response.data.message);
-  }
-};
-
 function ChatListPage() {
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
   const [auth] = useRecoilState(authState);
   const [chatRoom, setChatRoom] = useRecoilState(chatRoomState);
 
-  const { data, error, isLoading } = useQuery({
-    queryKey: [process.env.REACT_APP_QUERY_KEY],
-    queryFn: () => fetchData(Number(auth.userId), setChatRoom),
-    retry: 2
-  });
+  const fetchData = async () => {
+    try {
+      const response = await axiosInstance.get(`/chatrooms`, {
+        params: {
+          userId: auth.userId || 0
+        }
+      });
+      const chatRooms = {};
+      response.data.map(e => (chatRooms[`ch_${e.id}`] = e.status.status));
+      setChatRoom({ rooms: chatRooms });
+      setData(response.data);
+    } catch (err) {
+      setError(err.response.data.message);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   return (
     <MainContainer>
       <Head>
         <span>상담 요청 목록</span>
       </Head>
-      {error && <Notice>{error.message}</Notice>}
+      {error && <Notice>{error}</Notice>}
       {isLoading && <Notice>로딩 중 입니다..</Notice>}
       {data && data.map(e => <ChatRoomDetail key={e.id} data={e} />)}
     </MainContainer>
@@ -49,7 +52,8 @@ const Head = styled.p`
   display: flex;
   justify-content: center;
   align-items: center;
-  width: 95%;
+  max-width: 700px;
+  width: 90%;
   height: 4dvh;
   border: 1px solid black;
   text-align: center;
