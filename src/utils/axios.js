257d565c20afebc875c2edbff5fb3b0bc5f1e528
[Cloud3] 권오민 <murloc1999@naver.com>
2024-07-27 17:44:02 +0900

    fix : cookie config

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,8 @@
 import axios, { request } from 'axios';
 
 export const axiosInstance = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL
+  baseURL: process.env.REACT_APP_API_BASE_URL,
+  withCredentials: true
   // headers: { Authorization: `Bearer ${localStorage.getItem('token')}` }
 });
 
