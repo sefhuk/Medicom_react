194e7cde57a86206aa96ce1b10141b8db93ae51f
sanghyuk <ish775800@gmail.com>
2024-08-01 15:33:11 +0900

    fix: 채팅방 입장 불가 문제 해결

--- src/components/chatRoom/ChatRoomDetail.jsx
+++ src/components/chatRoom/ChatRoomDetail.jsx
@@ -13,7 +13,7 @@ function ChatRoomDetail({ data, selectedIndex }) {
   const [isHovered, setIsHovered] = useState(false);
 
   const handleWrapperClick = () => {
-    if (data.type.type !== '서비스센터 상담') {
+    if (auth.role === 'ADMIN' && data.type.type !== '서비스센터 상담') {
       return;
     }
 
