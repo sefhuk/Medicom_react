47c10f2b7087974958a329f34ee05faa66762839
명광호 <rhkdgh930@naver.com>
2024-08-03 04:20:10 +0900

    merge into dev

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,40 @@
-// import React from 'react';
-// import { Outlet, Link } from 'react-router-dom';
-// import './App.css';
-// import { NavermapsProvider } from 'react-naver-maps';
-//
-//
-// //import React, { useState } from 'react';
-//
-//
-//
-// const App = () => {
-//
-//   return (
-//
-//     <NavermapsProvider ncpClientId='327ksyij3n'>
-//       <div>
-//         {/* <nav>
-//           <ul>
-//             <li>
-//               <Link to="/">Home</Link>
-//             </li>
-//             <li>
-//               <Link to="/hospitals">Hospitals</Link>
-//             </li>
-//             <li>
-//               <Link to="/hospitals/maps">Maps</Link>
-//             </li>
-//           </ul>
-//         </nav> */}
-//         <Outlet />
-//       </div>
-//     </NavermapsProvider>
-//
-//
-//
-//
-//   );
-// };
-//
-// export default App;
-
 import React from 'react';
-import { Outlet } from 'react-router-dom';
-import Footer from './components/Footer';
+import { Outlet, Link } from 'react-router-dom';
+import './App.css';
+import { NavermapsProvider } from 'react-naver-maps';
 
-function App() {
+
+//import React, { useState } from 'react';
+
+
+
+const App = () => {
+
   return (
-    <div>
-      <Outlet />
-    </div>
+
+    <NavermapsProvider ncpClientId='327ksyij3n'>
+      <div>
+        {/* <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/hospitals">Hospitals</Link>
+            </li>
+            <li>
+              <Link to="/hospitals/maps">Maps</Link>
+            </li>
+          </ul>
+        </nav> */}
+        <Outlet />
+      </div>
+    </NavermapsProvider>
+
+
+
+
   );
-}
+};
 
 export default App;
