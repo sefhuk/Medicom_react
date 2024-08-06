ddbbc93c94fd54ba2638071a36f488057d5096cc
sanghyuk <ish775800@gmail.com>
2024-08-06 16:01:27 +0900

    style: MainContainer 스타일 수정 번복

--- src/components/global/MainContainer.jsx
+++ src/components/global/MainContainer.jsx
@@ -25,7 +25,6 @@ const Content = styled.div`
   flex: 1;
   height: ${({ isChat }) => (isChat === true ? '76dvh' : '84dvh')};
   margin-bottom: ${({ isChat }) => (isChat === true ? '8dvh' : '0px')};
-  overflow-y: scroll;
 `;
 
 export default MainContainer;
