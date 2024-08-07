fdf793ab7ee6215fa5fc08b526de743639e9b560
sanghyuk <ish775800@gmail.com>
2024-08-07 13:39:56 +0900

    feat: recoil sessionStorage key 값 수정

--- src/utils/atom.js
+++ src/utils/atom.js
@@ -2,7 +2,7 @@ import { atom } from 'recoil';
 import { recoilPersist } from 'recoil-persist';
 
 const { persistAtom } = recoilPersist({
-  key: 'sessionStorage',
+  key: 'recoilSession',
   storage: window.sessionStorage
 });
 
