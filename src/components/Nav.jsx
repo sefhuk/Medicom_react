2c0b6facf7f179ef9d8db7cfbc4c1f8c483cee57
narim <ehdrkd648@naver.com>
2024-08-14 23:03:47 +0900

    design: 뒤로가기 버튼

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,19 +1,19 @@
 import React, { useState } from 'react';
 import { AppBar, Toolbar, Typography, Button, Drawer, List, ListItem, ListItemButton, ListItemIcon, Box, ListItemText, Divider, IconButton } from '@mui/material';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
 import { useRecoilValue, useSetRecoilState } from 'recoil';
 import { chatRoomState, userauthState } from '../utils/atom';
 import { deleteCookie } from '../utils/cookies';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import HttpsIcon from '@mui/icons-material/Https';
 import { axiosInstance } from '../utils/axios';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
-import MenuIcon from '@mui/icons-material/Menu';
+import ArrowBack from '@mui/icons-material/ArrowBack';
+import Menu from '@mui/icons-material/Menu';
 
 const Nav = () => {
   const navigate = useNavigate();
   const auth = useRecoilValue(userauthState);
+  const location = useLocation();
   const setAuthState = useSetRecoilState(userauthState);
   const setChatRoomState = useSetRecoilState(chatRoomState);
   const [open, setOpen] = useState(false);
@@ -29,6 +29,10 @@ const Nav = () => {
 
   const handleHomePage = () => {
     navigate('/');
+  };
+
+  const handlePreviousPage = () => {
+    navigate(-1);
   };
 
   const handleLogoutClick = async () => {
@@ -68,37 +72,21 @@ const Nav = () => {
     navigate('/admin-page');
   }
 
+  const isHomePage = location.pathname === '/';
+
   return (
     <AppBar position="static" sx={{ bgcolor: 'white', textAlign: 'center', boxShadow: 'none' }}>
       <Toolbar>
-        {/* <IconButton
+      <IconButton
           size="large"
           edge="start"
-          color="inherit"
-          aria-label="menu"
-          onClick={toggleDrawer(true)}
+          color="black"
+          onClick={isHomePage ? handleHomePage : handlePreviousPage}
           sx={{ mr: 2 }}
         >
-          <MenuIcon />
-        </IconButton> */}
-        {/* <Drawer anchor="left"
-          open={open} onClose={toggleDrawer(false)}
-          sx={{
-            maxWidth: '60dvh',
-            overflow: 'hidden',
-            margin: 'auto',
-            '& .MuiBackdrop-root': {
-              margin: 'auto',
-            },
-            '& .MuiPaper-root': {
-              position: 'absolute'
-            }
-          }}
-        >
-        </Drawer> */}
-        {/* <Typography variant="h6" sx={{ flexGrow: 1 }}>
-          네비게이션바
-        </Typography> */}
+          {isHomePage ? <Menu /> : <ArrowBack />}
+        </IconButton>
+
         
         <Box sx = {{my: 2, cursor: 'pointer'}} onClick={handleHomePage}>
           <img src='/images/Group_Logo.svg'/>
