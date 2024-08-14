da503dde76eb00515178db6f596e3b9d486a9e2b
명광호 <rhkdgh930@naver.com>
2024-08-14 22:20:33 +0900

    before pull

--- src/components/board/PostSearchBar.jsx
+++ src/components/board/PostSearchBar.jsx
@@ -14,33 +14,33 @@ const PostSearchBar = ({ onSearch }) => {
             if (searchTerm) {
                 if (searchType === 'title') {
                     params.title = searchTerm;
-                    response = await axiosInstance.get('/posts/search', { params });
-                } else {
+                    response = await axiosInstance.get('/posts/searchByTitle', { params });
+                } else if (searchType === 'userName') {
                     params.userName = searchTerm;
                     response = await axiosInstance.get('/posts/searchByUserName', { params });
                 }
             } else {
-
+                // 검색어가 없을 경우 전체 포스트를 가져옴
                 response = await axiosInstance.get('/posts', { params });
             }
 
             if (response.data.content.length === 0) {
-                alert("해당 제목을 찾을 수 없습니다.");
+                alert("검색 결과가 없습니다.");
             } else {
                 onSearch(response.data);
             }
         } catch (error) {
             if (error.response && error.response.data) {
-                const { codeName, message } = error.response.data;
+                const { codeName } = error.response.data;
                 if (codeName === "USER_NOT_FOUND") {
-                    alert(`존재하지 않는 아이디 입니다.`);
+                    alert("존재하지 않는 사용자입니다.");
                 } else {
                     console.error("Error fetching search results:", error);
-                    alert("An error occurred while fetching search results.");
+                    alert("검색 중 오류가 발생했습니다.");
                 }
             } else {
                 console.error("Error fetching search results:", error);
-                alert("An unexpected error occurred.");
+                alert("예기치 못한 오류가 발생했습니다.");
             }
         }
     };
@@ -52,17 +52,21 @@ const PostSearchBar = ({ onSearch }) => {
                 onChange={(e) => setSearchType(e.target.value)}
                 sx={{ mr: 1 }}
             >
-                <MenuItem value="title">Title</MenuItem>
-                <MenuItem value="userName">User Name</MenuItem>
+                <MenuItem value="title">제목</MenuItem>
+                <MenuItem value="userName">사용자 이름</MenuItem>
             </Select>
             <TextField
                 variant="outlined"
-                label={`Search by ${searchType === 'title' ? 'Title' : 'User Name'}`}
+                label={`${
+                    searchType === 'title' ? '제목으로 검색' : '사용자 이름으로 검색'
+                }`}
                 value={searchTerm}
                 onChange={(e) => setSearchTerm(e.target.value)}
                 sx={{ mr: 1 }}
             />
-            <Button variant="contained" color="primary" onClick={handleSearch}>Search</Button>
+            <Button variant="contained" color="primary" onClick={handleSearch}>
+                검색
+            </Button>
         </Box>
     );
 };
